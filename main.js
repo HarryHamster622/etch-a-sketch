ab1cc45e184606c3bc0a5094ba42c4404334400a
HarryHamster622 <56613758+HarryHamster622@users.noreply.github.com>
2024-02-08 19:25:31 -0500

    Create main.css Create logic for initial grid, and creating grid of various sizes Make prettier

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 let gridContainer = document.querySelector('#grid');
 
-let sideLength = prompt('How long would you like the sides of the grid to be?');
+let sideLength = 8;
 
-if (sideLength > 99) {
+createGrid(sideLength);
+
+function redrawGrid() {
+
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    sideLength = prompt('How long would you like the sides of the grid to be?');
+
+    if (sideLength > 99) {
     sideLength = 99;
+    }   
+
+    createGrid(sideLength)
 }
 
-for (i = 0; i < sideLength; i++) {
-    let gridRow = document.createElement('div');
-    gridRow.classList.add('gridRow')
-    for (j = 0; j < sideLength; j++) {
-        let gridSpot = document.createElement('div')
-        gridRow.appendChild(gridSpot);
+function createGrid(sL) {
+    for (i = 0; i < sL; i++) {
+        let gridRow = document.createElement('div');
+        gridRow.classList.add('gridRow')
+        for (j = 0; j < sL; j++) {
+            let gridSpot = document.createElement('div')
+            gridSpot.classList.add('gridSpot');
+            gridRow.appendChild(gridSpot);
+        }
+        gridContainer.appendChild(gridRow);
     }
-    gridContainer.appendChild(gridRow);
 }
